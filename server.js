a2056c1b7d0bd4c503eaa4e53c31e1c9e3c19741
Sebastian Clavijo <sebas.cj@gmail.com>
2018-03-15 00:07:22 -0500

    Added sockets io client and server

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
+
+// app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/dist'));
+
 // app.set('view engine', 'html');
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 5000);
+// app.listen(process.env.PORT || 3000);
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function() {
+    console.log('Alguien se ha conectado con sockets');
+});
+
+server.listen(8080, function() {
+    console.log("Servidor corriendo en http://localhost:8080");
+});
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {GameService} from './game.service';
 import {Subscription} from 'rxjs/Subscription';
 import {Observable} from 'rxjs/Observable';
+import io from 'socket.io-client';
 
 @Component({
   selector: 'app-game',
@@ -13,13 +14,17 @@ export class GameComponent implements OnInit, OnDestroy {
   selectedGameSubscription$: Subscription;
   onGoingGame = false;
   // message: string;
-  constructor( public gameService: GameService ) {}
+  constructor( public gameService: GameService ) {
+        var socket = io.connect('http://localhost:8080', {'forceNew': true});
+    }
 
 
   ngOnInit() {
     // this.gameService.testMessage().subscribe(data => this.message = data.msg);
     // console.log(this.message);
     this.selectedGameSubscription$ = this.gameService.OngoingGame$.subscribe(game => { this.onGoingGame = game; });
+
+
   }
 
   ngOnDestroy() {
@@ -32,5 +37,3 @@ export class GameComponent implements OnInit, OnDestroy {
   }
 
 }
-
-
